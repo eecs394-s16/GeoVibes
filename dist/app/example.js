de051941db1dce138ffc0a493c2108928ab27495
babaozhou <babaozhou126@126.com>
2016-04-05 18:57:25 -0500

    readJson

--- dist/app/example.js
+++ dist/app/example.js
@@ -1,7 +1,148 @@
-angular.module('example', [
-  // Declare here all AngularJS dependencies that are shared by the example module.
+var GeoVibesApp = angular.module('example', [
+  // Declare here all AngularJS dependencies that are shared by all modules.
   'supersonic'
 ]);
+
+GeoVibesApp.controller('HomeController', function($scope, supersonic) {
+
+    $scope.geoVibesTitle = "GeoVibes";
+
+
+     function loadJSON() {   
+
+         var xobj = new XMLHttpRequest();
+         
+         xobj.open("GET", 'http://tenaciousj.github.io/sampleTwitterEndpoint/sample-twitter-response.json', true);
+         // $scope.json_test = xobj.status;
+         $scope.json_test = xobj.status;
+         xobj.onreadystatechange = function(data) {
+          // document.getElementById("aaa").innerHTML = "12321232323232";
+          if (xobj.readyState == 4 && xobj.status == "200"){
+            var json = JSON.parse(xobj.responseText);
+            document.getElementById("aaa").innerHTML = "12321";
+            $scope.json_test = "yay";
+            // $scope.$apply();
+          }
+          else{
+            $scope.json_test = "fail";
+            // $scope.$apply();
+            document.getElementById("aaa").innerHTML = "12321";
+          }
+         }
+         $scope.$apply();
+         // document.getElementById("aaa").innerHTML = "end";
+       }
+
+
+    function initializeMap() {
+      loadJSON();
+
+      //neighborhoods of chicago
+      var chicago=new google.maps.LatLng(41.8781,-87.6298);
+      var lakeview=new google.maps.LatLng(41.9436,-87.6584);
+      var uptown=new google.maps.LatLng(41.9665,-87.6533);
+      var evanston=new google.maps.LatLng(42.0451,-87.6877);
+      var wickerpark=new google.maps.LatLng(41.9088,-87.6796);
+      var streeterville=new google.maps.LatLng(41.8927,-87.6200);
+
+      var mapProp = {
+        center:chicago,
+        zoom:10,
+          panControl:true,
+        zoomControl:true,
+        mapTypeControl:true,
+        scaleControl:true,
+        streetViewControl:true,
+        overviewMapControl:true,
+        rotateControl:true, 
+        mapTypeId:google.maps.MapTypeId.ROADMAP
+      };
+        // Create the search box and link it to the UI element.
+      var input = document.getElementById('pac-input');
+      var searchBox = new google.maps.places.SearchBox(input);
+
+
+      var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+      var lakeviewcirc = new google.maps.Circle({
+      center:lakeview,
+      radius:3000,
+      strokeColor:"yellow",
+      strokeOpacity:0.8,
+      strokeWeight:2,
+      fillColor:"yellow",
+      fillOpacity:0.4
+    });
+        var uptowncirc = new google.maps.Circle({
+      center:uptown,
+      radius:3000,
+      strokeColor:"orange",
+      strokeOpacity:0.8,
+      strokeWeight:2,
+      fillColor:"orange",
+      fillOpacity:0.4
+    });
+         var evanstoncirc = new google.maps.Circle({
+      center:evanston,
+      radius:3000,
+      strokeColor:"purple",
+      strokeOpacity:0.8,
+      strokeWeight:2,
+      fillColor:"purple",
+      fillOpacity:0.4
+    });
+         var wickerparkcirc = new google.maps.Circle({
+      center:wickerpark,
+      radius:3000,
+      strokeColor:"turquoise",
+      strokeOpacity:0.8,
+      strokeWeight:2,
+      fillColor:"turquoise",
+      fillOpacity:0.4
+    });
+        var streetervillecirc = new google.maps.Circle({
+      center:streeterville,
+      radius:3000,
+      strokeColor:"red",
+      strokeOpacity:0.8,
+      strokeWeight:2,
+      fillColor:"red",
+      fillOpacity:0.4
+    });
+        lakeviewcirc.setMap(map);
+        uptowncirc.setMap(map);
+        evanstoncirc.setMap(map);
+        wickerparkcirc.setMap(map);
+        streetervillecirc.setMap(map);
+        
+        var infowindow = new google.maps.InfoWindow({
+      content:"#timeoutchicago"
+      });
+
+    google.maps.event.addListener(lakeviewcirc, 'click', function(ev){
+        infowindow.setPosition(ev.latLng);
+        infowindow.open(map);
+    });
+        google.maps.event.addListener(uptowncirc, 'click', function(ev){
+        infowindow.setPosition(ev.latLng);
+        infowindow.open(map);
+    });
+        google.maps.event.addListener(evanstoncirc, 'click', function(ev){
+        infowindow.setPosition(ev.latLng);
+        infowindow.open(map);
+    });
+        google.maps.event.addListener(wickerparkcirc, 'click', function(ev){
+        infowindow.setPosition(ev.latLng);
+        infowindow.open(map);
+    });
+         google.maps.event.addListener(streetervillecirc, 'click', function(ev){
+        infowindow.setPosition(ev.latLng);
+        infowindow.open(map);
+    });
+    }
+        
+    google.maps.event.addDomListener(window, 'load', initializeMap);
+
+  });
 
 angular
   .module('example')
