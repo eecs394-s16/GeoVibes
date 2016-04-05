95de778574a0919c00f7a0dc2daa5c8dd2c7ebcf
jeanettepranin <jeanettepranin@gmail.com>
2016-04-05 13:19:10 -0500

    delete routing for now, see if it deploys

--- dist/app/common.js
+++ dist/app/common.js
@@ -1,4 +1,21 @@
-angular.module('common', [
+var GeoVibesApp = angular.module('common', [
   // Declare here all AngularJS dependencies that are shared by all modules.
   'supersonic'
 ]);
+
+// //Define Routing for app
+// GeoVibesApp.config(['$routeProvider',
+//   function($routeProvider) {
+//     $routeProvider.
+//       when('/map', {
+//         templateUrl: 'views/map.html',
+//         controller: 'MapController'
+//       }).
+//       otherwise({
+//         redirectTo: '/map'
+//       });
+// }]);
+
+// GeoVibesApp.controller('MapController', function($scope){
+
+// });
