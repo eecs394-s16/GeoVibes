95bceafb4502f121dd539df449f54fef8dd773d5
jeanettepranin <jeanettepranin@gmail.com>
2016-04-05 17:15:30 -0500

    setup angular in dindex.js

--- app/example/index.js
+++ app/example/index.js
@@ -1,4 +1,22 @@
-angular.module('example', [
-  // Declare here all AngularJS dependencies that are shared by the example module.
+var GeoVibesApp = angular.module('example', [
+  // Declare here all AngularJS dependencies that are shared by all modules.
   'supersonic'
 ]);
+
+//Define Routing for app
+GeoVibesApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/home', {
+        templateUrl: 'views/layout.html',
+        controller: 'HomeController'
+      }).
+      otherwise({
+        redirectTo: '/home'
+      });
+}]);
+
+GeoVibesApp.controller('HomeController', function($scope){
+	
+	
+});
