fa082df411d9ff2d05fbcbfea2f3c8e499b69e8a
jeanettepranin <jeanettepranin@gmail.com>
2016-04-11 15:29:45 -0500

    comment out rosalie's for now

--- app/example/index.js
+++ app/example/index.js
@@ -116,6 +116,7 @@ GeoVibesApp.controller('HomeController', function($scope, supersonic) {
         // Create the search box and link it to the UI element.
         var input = document.getElementById('pac-input');
         var searchBox = new google.maps.places.SearchBox(input);
+        var LgdIcon = document.getElementById('LegendIcon');
 
         var userLoc = new google.maps.LatLng(userLat,userLong);
 
@@ -134,6 +135,12 @@ GeoVibesApp.controller('HomeController', function($scope, supersonic) {
         };
 
         var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+        map.controls[google.maps.ControlPosition.RIGHT_TOP].push(LgdIcon);
+
+        // Bias the SearchBox results towards current map's viewport.
+        map.addListener('bounds_changed', function() {
+          searchBox.setBounds(map.getBounds());
+        });
 
         for(var t = 0; t < tweets.length; t++){
           var latLongPair = new google.maps.LatLng(tweets[t]["latitude"],tweets[t]["longitude"]);
@@ -157,6 +164,52 @@ GeoVibesApp.controller('HomeController', function($scope, supersonic) {
           });
 
         }
+
+
+        // var markers = [];
+        // // Listen for the event fired when the user selects a prediction and retrieve
+        // // more details for that place.
+        // searchBox.addListener('places_changed', function() {
+        //   var places = searchBox.getPlaces();
+
+        //   if (places.length == 0) {
+        //     return;
+        //   }
+
+        //   // Clear out the old markers.
+        //   markers.forEach(function(marker) {
+        //     marker.setMap(null);
+        //   });
+        //   markers = [];
+
+        //   // For each place, get the icon, name and location.
+        //   var bounds = new google.maps.LatLngBounds();
+        //   places.forEach(function(place) {
+        //     var icon = {
+        //       url: place.icon,
+        //       size: new google.maps.Size(71, 71),
+        //       origin: new google.maps.Point(0, 0),
+        //       anchor: new google.maps.Point(17, 34),
+        //       scaledSize: new google.maps.Size(25, 25)
+        //     };
+
+        //     // Create a marker for each place.
+        //     markers.push(new google.maps.Marker({
+        //       map: map,
+        //       icon: icon,
+        //       title: place.name,
+        //       position: place.geometry.location
+        //     }));
+
+        //     if (place.geometry.viewport) {
+        //       // Only geocodes have viewport.
+        //       bounds.union(place.geometry.viewport);
+        //     } else {
+        //       bounds.extend(place.geometry.location);
+        //     }
+        //   });
+        //   map.fitBounds(bounds);
+        // });
 
     });
   };
