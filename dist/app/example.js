a5918831b52569fa01ebb8b6547cce3022a4eb56
Rosalie Chan <rosaliemchan@yahoo.com>
2016-04-12 11:54:52 -0500

    fixed search bar

--- dist/app/example.js
+++ dist/app/example.js
@@ -139,6 +139,39 @@ GeoVibesApp.controller('HomeController', function($scope, supersonic) {
         map.addListener('bounds_changed', function() {
           searchBox.setBounds(map.getBounds());
         });
+          
+         // Listen for the event fired when the user selects a prediction and retrieve
+         // more details for that place.
+         searchBox.addListener('places_changed', function() {
+           var places = searchBox.getPlaces();
+
+           if (places.length == 0) {
+             return;
+           }
+
+           // For each place, get the icon, name and location.
+           var bounds = new google.maps.LatLngBounds();
+           places.forEach(function(place) {
+             var icon = {
+               url: place.icon,
+               size: new google.maps.Size(71, 71),
+               origin: new google.maps.Point(0, 0),
+               anchor: new google.maps.Point(17, 34),
+               scaledSize: new google.maps.Size(25, 25)
+             };
+
+             if (place.geometry.viewport) {
+               // Only geocodes have viewport.
+               bounds.union(place.geometry.viewport);
+             } else {
+               bounds.extend(place.geometry.location);
+             }
+           });
+           map.fitBounds(bounds);
+         });
+
+          
+          
         console.info(tweets.length + "circle");
         for(var t = 0; t < tweets.length; t++){
 
@@ -168,53 +201,6 @@ GeoVibesApp.controller('HomeController', function($scope, supersonic) {
           })(location,tweets[t]["content"],infowindow, latLongPair));
         }
         
-
-
-        // var markers = [];
-        // // Listen for the event fired when the user selects a prediction and retrieve
-        // // more details for that place.
-        // searchBox.addListener('places_changed', function() {
-        //   var places = searchBox.getPlaces();
-
-        //   if (places.length == 0) {
-        //     return;
-        //   }
-
-        //   // Clear out the old markers.
-        //   markers.forEach(function(marker) {
-        //     marker.setMap(null);
-        //   });
-        //   markers = [];
-
-        //   // For each place, get the icon, name and location.
-        //   var bounds = new google.maps.LatLngBounds();
-        //   places.forEach(function(place) {
-        //     var icon = {
-        //       url: place.icon,
-        //       size: new google.maps.Size(71, 71),
-        //       origin: new google.maps.Point(0, 0),
-        //       anchor: new google.maps.Point(17, 34),
-        //       scaledSize: new google.maps.Size(25, 25)
-        //     };
-
-        //     // Create a marker for each place.
-        //     markers.push(new google.maps.Marker({
-        //       map: map,
-        //       icon: icon,
-        //       title: place.name,
-        //       position: place.geometry.location
-        //     }));
-
-        //     if (place.geometry.viewport) {
-        //       // Only geocodes have viewport.
-        //       bounds.union(place.geometry.viewport);
-        //     } else {
-        //       bounds.extend(place.geometry.location);
-        //     }
-        //   });
-        //   map.fitBounds(bounds);
-        // });
-
     });
 
 
