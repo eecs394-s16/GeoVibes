ea62bebf1ea80f946028e36dbb0724bd9574ab3d
Eli <elicohen2018@u.northwestern.edu>
2016-04-12 10:46:21 -0500

    added apis

--- app.js
+++ app.js
@@ -7,29 +7,7 @@ var express = require('express'),
 
   var port = process.env.PORT || 8000;
 
-  // //Get this data from your twitter apps dashboard
-  // var client = new Twitter({
-  //     "consumerKey": config.consumerKey,
-  //     "consumerSecret": config.consumerSecret,
-  //     "accessToken": config.accessToken,
-  //     "accessTokenSecret": config.accessTokenSecret,
-  //     "callBackUrl": "http://placeholder.com"
-  // });
- 
-  // var params = {screen_name: 'elicohen2018'};
-
-  // client.get('statuses/user_timeline', params, function(error, tweets, response){
-  //   if (!error) {
-  //     console.log(tweets);
-  //     t = tweets;
-  //   } else {
-  //     console.log(error);
-  //   }
-  // });
-
-
-
-  app.get('/',function(req,res){
+  app.get('/search/tweets',function(req,res){
 
     var oauth = new OAuth.OAuth(
       'https://api.twitter.com/oauth/request_token',
@@ -42,15 +20,41 @@ var express = require('express'),
     );
 
     oauth.get(
-      'https://api.twitter.com/1.1/geo/search.json?query=Evanston',
+      'https://api.twitter.com/1.1/search/tweets?query='+req.query.query,
       config.accessToken, //test user token
-      config.accessTokenSecret, //test user secret            
+      config.accessTokenSecret, //test user secret
       function (e, data, result){
-        if (e) console.error(e);   
+        if (e) console.error(e);
         res.json(data);
-        console.log(data);   
-        // console.log(require('util').inspect(data));    
-      });
-  })
+        console.log(data);
+        // console.log(require('util').inspect(data));
+      }
+    );
+  });
+
+  app.get('/geo/reverse_geocode',function(req,res){
+
+    var oauth = new OAuth.OAuth(
+      'https://api.twitter.com/oauth/request_token',
+      'https://api.twitter.com/oauth/access_token',
+      config.consumerKey,
+      config.consumerSecret,
+      '1.0A',
+      null,
+      'HMAC-SHA1'
+    );
+
+    oauth.get(
+      'https://api.twitter.com/1.1/geo/reverse_geocode?lat='+req.query.lat+'&long='+req.query.long,
+      config.accessToken, //test user token
+      config.accessTokenSecret, //test user secret
+      function (e, data, result){
+        if (e) console.error(e);
+        res.json(data);
+        console.log(data);
+        // console.log(require('util').inspect(data));
+      }
+    );
+  });
 
   app.listen(port);
