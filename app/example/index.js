6dfddc33d37df0aa2f4c8cdfe308fa3b0e13dec8
Rosalie Chan <rosaliemchan@yahoo.com>
2016-04-20 21:44:18 -0500

    made search box work

--- app/example/index.js
+++ app/example/index.js
@@ -101,12 +101,46 @@ GeoVibesApp.controller('HomeController', function($scope, supersonic) {
     	  var input = document.getElementById("pac-input");
     	  var searchBox = new google.maps.places.SearchBox(input);
     	  
-    	  searchBox.addListener('places_changed', function(){
-    		  var places = searchBox.getPlaces();
-          var name = places[0].name;
-          getTweetsFromLocation(name);
-    	  });
+//    	  searchBox.addListener('places_changed', function(){
+//    		  var places = searchBox.getPlaces();
+//          var name = places[0].name;
+//          getTweetsFromLocation(name);
+//    	  });
+//
+	  map.addListener('bounds_changed', function() {
+          searchBox.setBounds(map.getBounds());
+        });
 
-	  
+         // Listen for the event fired when the user selects a prediction and retrieve
+         // more details for that place.
+         searchBox.addListener('places_changed', function() {
+           var places = searchBox.getPlaces();
+             
+           if (places.length == 0) {
+             return;
+           }
+             var name = places[0].name;
+          getTweetsFromLocation(name);
+
+           // For each place, get the icon, name and location.
+           var bounds = new google.maps.LatLngBounds();
+           places.forEach(function(place) {
+             var icon = {
+               url: place.icon,
+               size: new google.maps.Size(71, 71),
+               origin: new google.maps.Point(0, 0),
+               anchor: new google.maps.Point(17, 34),
+               scaledSize: new google.maps.Size(25, 25)
+             };
+
+             if (place.geometry.viewport) {
+               // Only geocodes have viewport.
+               bounds.union(place.geometry.viewport);
+             } else {
+               bounds.extend(place.geometry.location);
+             }
+           });
+           map.fitBounds(bounds);
+         });
    }
  });
