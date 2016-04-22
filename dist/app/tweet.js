3187452f74b5aea7635d7b7e3e6fef4cb5495cfd
babaozhou <leizhou2015@u.northwestern.edu>
2016-04-21 20:23:16 -0500

    data update

--- dist/app/tweet.js
+++ dist/app/tweet.js
@@ -35,7 +35,17 @@ Index.controller("IndexController", function ($scope, supersonic) {
 
     var test;
     var Tweet = supersonic.data.model("Tweet");
-    Tweet.findAll().then(function(allTweets){
+//     Tweet.findAll().then(function(allTweets){
+//         test = "<ul>";
+//         for(var t = 0; t < allTweets.length; t++){
+// //            $scope.tweets.push(allTweets[t]["content"] + allTweets[t]["username"]);
+//             test += "<li>"+allTweets[t]["username"] + ": " + allTweets[t]["content"] + "</li><hr>";
+//         }
+//         test += "</ul>";
+//         console.info("aaa" +allTweets);
+//         document.getElementById("aaaa").innerHTML = test;
+//     });
+    Tweet.all().whenChanged( function (allTweets) {
         test = "<ul>";
         for(var t = 0; t < allTweets.length; t++){
 //            $scope.tweets.push(allTweets[t]["content"] + allTweets[t]["username"]);
