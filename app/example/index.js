884c9bc4194dcedf0ba20f938b9199a31bcb072d
Eli <elicohen2018@u.northwestern.edu>
2016-04-24 21:50:48 -0500

    cleared markers

--- app/example/index.js
+++ app/example/index.js
@@ -133,9 +133,10 @@ GeoVibesApp.controller('HomeController', function($scope, supersonic) {
         return;
       }
       // Clear out the old markers.
-      markers.forEach(function(marker) {
-        marker.setMap(null);
-      });
+      // markers.forEach(function(marker) {
+      //   marker.setMap(null);
+      // });
+      setMapOnAll(null);
       markers = [];
 
       var name = places[0].name;
